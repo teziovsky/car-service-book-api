af032f573881890ba6442d958e4d6b212c5a4a29
Jakub Soboczyński <teziovsky@gmail.com>
2022-07-27 13:01:31 +0200

    refactor[backend]: update PORT env variable

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,10 @@
 import { ValidationPipe } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get<ConfigService>(ConfigService);
   app.setGlobalPrefix("api");
 
   const config = new DocumentBuilder()
@@ -23,7 +21,7 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
-  await app.listen(configService.get("API_PORT") || 3000);
+  await app.listen(3000);
 }
 
 bootstrap();
