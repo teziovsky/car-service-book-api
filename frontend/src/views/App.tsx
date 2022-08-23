43b44ac2ca3464ee2cca47ea210eba9db76edd81
Jakub Soboczyński <teziovsky@gmail.com>
2022-08-23 21:24:12 +0200

    feat: add react router

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -1,24 +1,16 @@
-import Button from "components/Button/Button";
-import Heading from "components/Heading/Heading";
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-function App() {
-  const [selectedTheme, setSelectedTheme] = useState("light");
+import Index from "./Index";
 
+const App = () => {
   return (
-    <div>
-      <Heading level={1}>{selectedTheme}</Heading>
-      <Heading level={2}>Button</Heading>
-      <div style={{ display: "flex", gap: "8px" }}>
-        <Button onClick={() => setSelectedTheme("light")} icon="HiPlus">
-          Light
-        </Button>
-        <Button onClick={() => setSelectedTheme("dark")} secondary>
-          Dark
-        </Button>
-      </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
