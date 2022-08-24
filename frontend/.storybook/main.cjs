9341ed26dacd07349a7d5755787497f83eac6759
Jakub Soboczyński <teziovsky@gmail.com>
2022-08-24 17:13:02 +0200

    feat(storybook): add router and aliases

--- frontend/.storybook/main.cjs
+++ frontend/.storybook/main.cjs
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: [
     "../src/**/*.stories.mdx",
@@ -7,6 +9,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-addon-react-router-v6"
   ],
   framework: "@storybook/react",
   core: {
@@ -14,5 +17,30 @@ module.exports = {
   },
   features: {
     storyStoreV7: true,
+  },
+  async viteFinal(config, { configType }) {
+    return {
+      ...config,
+      resolve: {
+        alias: [
+          {
+            find: "components",
+            replacement: path.resolve(__dirname, "../src/components"),
+          },
+          {
+            find: "layouts",
+            replacement: path.resolve(__dirname, "../src/layouts"),
+          },
+          {
+            find: "routes",
+            replacement: path.resolve(__dirname, "../src/routes"),
+          },
+          {
+            find: "views",
+            replacement: path.resolve(__dirname, "../src/views"),
+          },
+        ],
+      },
+    };
   },
 };
--- frontend/.storybook/preview.cjs
+++ frontend/.storybook/preview.cjs
@@ -1,17 +1,22 @@
+import { withRouter } from "storybook-addon-react-router-v6";
 import "../src/main.scss";
+
+export const decorators = [withRouter];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
-      color: /(background|color)$/i,
       date: /Date$/,
     },
   },
   layout: "centered",
   options: {
     storySort: {
-      method: "alphabetical"
-    }
+      method: "alphabetical",
+    },
+  },
+  reactRouter: {
+    routePath: "/",
   },
 };
