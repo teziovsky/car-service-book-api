75e8090994c0073643f8972c5cf060baec17297a
Jakub Soboczyński <teziovsky@gmail.com>
2022-08-28 18:29:00 +0200

    feat(header): add component

---
+++ frontend/src/components/Layouts/Header/Header.styled.ts
@@ -0,0 +1,73 @@
+import Button from "components/Elements/Button/Button";
+import Heading from "components/Elements/Heading/Heading";
+import { Font, Hsla, Transition } from "components/Styled/Mixins";
+import { Link } from "react-router-dom";
+import styled, { css } from "styled-components";
+
+export const StyledHeader = styled.header`
+  position: sticky;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
+
+  ${({ theme }) =>
+    css`
+      background-color: ${Hsla(theme.accent950)};
+    `}
+`;
+
+export const StyledLogo = styled(Link)`
+  position: absolute;
+  z-index: 11;
+  top: 55%;
+  left: 50%;
+  text-decoration: none;
+  transform: translate(-50%, -50%);
+
+  ${({ theme }) =>
+    css`
+      border-radius: ${theme.borderRadius};
+      &:hover {
+        .headerTitle {
+          color: ${Hsla(theme.accent400)};
+        }
+      }
+    `}
+`;
+
+export const StyledTitle = styled(Heading)`
+  display: flex;
+  flex-direction: column;
+  ${Transition("color")};
+
+  ${({ theme }) =>
+    css`
+      color: ${Hsla(theme.accent200)}; ;
+    `}
+`;
+
+export const StyledTitleHeading = styled.span`
+  ${({ theme }) =>
+    css`
+      ${Font({ size: 28, lineHeight: 32, fontFamily: theme.familyLogo })};
+
+      letter-spacing: 0.02em;
+    `}
+`;
+
+export const StyledTitleSubheading = styled.span`
+  ${({ theme }) =>
+    css`
+      ${Font({ size: 18, lineHeight: 22, fontFamily: theme.familyLogo })};
+
+      letter-spacing: -0.02em;
+      margin-top: -8px;
+    `}
+`;
+
+export const StyledButton = styled(Button)``;
--- frontend/src/components/Layouts/Header/Header.tsx
+++ frontend/src/components/Layouts/Header/Header.tsx
@@ -1,44 +1,42 @@
-import cx from "classnames";
 import BurgerMenu from "components/Elements/BurgerMenu/BurgerMenu";
-import Button from "components/Elements/Button/Button";
-import Heading from "components/Elements/Heading/Heading";
+import {
+  StyledButton,
+  StyledHeader,
+  StyledLogo,
+  StyledTitle,
+  StyledTitleHeading,
+  StyledTitleSubheading,
+} from "components/Layouts/Header/Header.styled";
 import Navigation, { LinkType } from "components/Layouts/Navigation/Navigation";
 import React, { FC, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { DefaultProps } from "src/main";
-
-import styles from "./Header.module.scss";
 
 type Props = DefaultProps & {
   type: "Home" | "App";
   links: LinkType[];
 };
 
-const Header: FC<Props> = ({ type, links, className: passedClasses, ...props }) => {
+const Header: FC<Props> = ({ type, links, ...props }) => {
   const [isOpen, setIsOpen] = useState(false);
   const route = useNavigate();
 
   return (
-    <header className={cx(styles.header, passedClasses)} {...props}>
+    <StyledHeader {...props}>
       <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
       {!!links.length && <Navigation isOpen={isOpen} links={links} />}
-      <Link className={cx(styles.headerLogo)} to="/">
-        <Heading className={cx(styles.headerTitle)} level={1}>
-          <span className={cx(styles.headerTitleHeading)}>Vehicle</span>
-          <span className={cx(styles.headerTitleSubheading)}>Service Book</span>
-        </Heading>
-      </Link>
+      <StyledLogo to="/">
+        <StyledTitle level={1}>
+          <StyledTitleHeading>Vehicle</StyledTitleHeading>
+          <StyledTitleSubheading>Service Book</StyledTitleSubheading>
+        </StyledTitle>
+      </StyledLogo>
       {type === "Home" && (
-        <Button
-          onClick={() => route("/app")}
-          color="accent"
-          className={styles.headerCta}
-          status="secondary"
-          size="small">
+        <StyledButton onClick={() => route("/app")} color="accent" status="secondary" size="small">
           Go to app
-        </Button>
+        </StyledButton>
       )}
-    </header>
+    </StyledHeader>
   );
 };
 
