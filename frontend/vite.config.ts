2575cba4d495d706a755b4cae52d2da9fe25b5c1
Jakub Soboczyński <teziovsky@gmail.com>
2022-09-12 12:06:05 +0200

    feat(vite): update host and port config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,15 @@
 import react from "@vitejs/plugin-react";
 import * as path from "path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default ({ mode }) => {
-  const { APP_PORT } = loadEnv(mode, path.resolve(__dirname, ".."), "APP");
-
+export default () => {
   return defineConfig({
     plugins: [react()],
     envDir: path.resolve(__dirname, ".."),
     server: {
-      port: parseInt(APP_PORT) || 3000,
+      host: true,
+      port: 3000,
     },
     resolve: {
       alias: {
