2c57a7952ae4650ee88327dd354764574631936f
Jakub Soboczyński <teziovsky@gmail.com>
2022-09-15 22:20:15 +0200

    feat: add services, types and plugins

---
+++ frontend/src/types/car.d.ts
@@ -0,0 +1,12 @@
+export interface Car {
+  id: number;
+  type?: string;
+  brand: string;
+  model: string;
+  generation?: string;
+  productionYear?: number;
+  engineType?: string;
+  engineCapacity?: string;
+  enginePower?: number;
+  gearboxType?: string;
+}
