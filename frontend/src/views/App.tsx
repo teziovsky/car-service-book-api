84572187e6f8f41f69787f3609e63d465afa7063
Jakub Soboczyński <teziovsky@gmail.com>
2022-09-15 22:16:06 +0200

    feat(tailwind): add tailwind css, post css and autoprefixer

--- frontend/src/views/App.tsx
+++ frontend/src/views/App.tsx
@@ -1,18 +1,9 @@
-import GlobalStyle from "components/Styled/Global";
-import useTheme from "hooks/useTheme";
-import React from "react";
+import "assets/css/index.css";
+
 import Router from "routes";
-import { ThemeProvider } from "styled-components";
 
 const App = () => {
-  const { theme } = useTheme();
-
-  return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-    </ThemeProvider>
-  );
+  return <Router />;
 };
 
 export default App;
---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
